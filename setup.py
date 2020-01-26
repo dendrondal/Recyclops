34603514f89586c34fa317ae17411549037cfa57
deterministic-forest <jwilli18@go.olemiss.edu>
2020-01-26 11:32:06 -0600

    Updated setup.py to manually find directories

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name="src",
     packages=find_packages(),
+    packages =['cif3r', 'app']
     version="0.1.0",
     description="Binary image classificether an image is of a recycleable or compostable object",
     author="Dal Williams",
