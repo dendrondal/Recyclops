0120d33c35b82f61350fc10b3ab880bbce046a8e
deterministic-forest <jwilli18@go.olemiss.edu>
2020-01-27 10:59:14 -0600

    Changed visualization to use pandas dataframe

--- cif3r/visualization/visualize.py
+++ cif3r/visualization/visualize.py
@@ -1,11 +1,12 @@
 import tensorflow as tf
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from pathlib import Path
 import seaborn as sb
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import sqlite3
-from cif3r.models.train_model import train_val_split
+from cif3r.models.train_model import datagen, process_path
 
 PARENT_DIR = Path(__file__).resolve().parents[2]
 KAGGLE_DATA_PATH = PARENT_DIR / 'data/raw/DATASET/TEST'
@@ -15,11 +16,14 @@ VIZ_DIR= PARENT_DIR / 'reports/figures'
 
 def plot_confusion_matrix(university:str):
     """For kaggle data, prediction class is organized by folder structure, but for scraped data, sql metadata is used."""
-    X_train, y_train, X_val, y_val = train_val_split(university)
-    X_val = X_val.reshape((64, 244, 244, 3))
-    clf = tf.keras.load_model(MODEL_DIR / university)
-    y_hat = clf.predict_classes(X_val)
-    con_mat = tf.math.confusion_matrix(labels=y_val, predictions=y_hat).numpy()
+    df = datagen(university)
+    X_val = df.sample(n=int(len(df)/5), random_state=42)
+    clf = tf.keras.models.load_model(MODEL_DIR / f'{university}.h5')
+    df['y_hat'] = clf.predict(process_path(df['filename']))
+    con_mat = tf.math.confusion_matrix(
+        labels=df['class'],
+        predictions=df['y_hat']
+        ).numpy()
     con_mat_norm = np.around(
         con_mat.astype('float') / con_mat.sum(axis=1)[:, np.newaxis], 
         decimals=2
