589fbe5a0159be78f0ed0e9a5e6ef37899b65518
deterministic-forest <jwilli18@go.olemiss.edu>
2020-01-27 17:42:21 -0600

    Added confusion matrix visualization

--- cif3r/visualization/visualize.py
+++ cif3r/visualization/visualize.py
@@ -2,11 +2,14 @@ import tensorflow as tf
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from pathlib import Path
 import seaborn as sb
+from sklearn.preprocessing import MultiLabelBinarizer
+from sklearn.metrics import confusion_matrix
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import sqlite3
-from cif3r.models.train_model import datagen, process_path
+from PIL import Image
+from cif3r.models.train_model import datagen, train_val_split, label_encoding
 
 PARENT_DIR = Path(__file__).resolve().parents[2]
 KAGGLE_DATA_PATH = PARENT_DIR / 'data/raw/DATASET/TEST'
@@ -14,27 +17,34 @@ MODEL_DIR = PARENT_DIR / 'models'
 VIZ_DIR= PARENT_DIR / 'reports/figures'
 
 
-def plot_confusion_matrix(university:str):
-    """For kaggle data, prediction class is organized by folder structure, but for scraped data, sql metadata is used."""
+def prediction_mapping(university:str):
+    try:
+        clf = tf.keras.models.load_model(MODEL_DIR / f'{university}.h5')
+    except OSError:
+        raise Exception(f"Unable to find model. Valid  models include {MODEL_DIR.glob('*.h5')}")
     df = datagen(university)
-    X_val = df.sample(n=int(len(df)/5), random_state=42)
-    clf = tf.keras.models.load_model(MODEL_DIR / f'{university}.h5')
-    df['y_hat'] = clf.predict(process_path(df['filename']))
-    con_mat = tf.math.confusion_matrix(
-        labels=df['class'],
-        predictions=df['y_hat']
-        ).numpy()
-    con_mat_norm = np.around(
-        con_mat.astype('float') / con_mat.sum(axis=1)[:, np.newaxis], 
-        decimals=2
-        )
-    con_mat_df = pd.DataFrame(con_mat_norm, index=classes, columns=classes)
+    df = df.sample(n=int(len(df)/10), random_state=42)
+    images = ImageDataGenerator().flow_from_dataframe(df, batch_size=64)
+    y_hat = list(clf.predict(images))
+    df['y_hat'] = y_hat
+    return {'df': df, 'labels':images.class_indices}
+
+    
+def plot_confusion_matrix(university:str):
+    """For kaggle data, prediction class is organized by folder structure, but for scraped data, 
+    sql metadata is used."""
+    preds = prediction_mapping(university)
+    df = preds['df']
+    df['y_hat'] = df['y_hat'].map(lambda x: np.where(x == np.amax(x))[0][0])
+    df['y_hat'] = df['y_hat'].map(lambda x: list(preds['labels'].keys())[x])
+    print(df.head())
+    labels = list(preds['labels'].keys())
+    con_mat = confusion_matrix(df['class'], df['y_hat'], labels=labels)
     figure = plt.figure(figsize=(8,8))
-    sns.heatmap(con_mat_df, annot=True, cmap=plt.cm.Blues)
+    con_mat_df = pd.DataFrame(con_mat, index=labels, columns=labels)
+    sb.heatmap(con_mat_df, annot=True, cmap=plt.cm.Blues)
     plt.tight_layout()
     plt.xlabel('True label')
     plt.ylabel('Predicted label')
-    plt.savefig(f'{university}_confusion_matrix.png')
-
-
+    plt.savefig(VIZ_DIR / f'{university}_confusion_matrix.png')
 
