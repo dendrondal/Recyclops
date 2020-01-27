1091f567ecc54669f0af227f864f32394db70814
deterministic-forest <jwilli18@go.olemiss.edu>
2020-01-27 07:57:13 -0600

    Added confusion matrix visualization

--- cif3r/visualization/visualize.py
+++ cif3r/visualization/visualize.py
@@ -1,21 +1,36 @@
-from sklearn.metrics import confusion_matrix
+import tensorflow as tf
 from pathlib import Path
-from tensorflow.keras.models import load_model
-from src.models.train_model import validation_generator
-from typing import Union
+import seaborn as sb
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
 import sqlite3
+from cif3r.models.train_model import train_val_split
 
 PARENT_DIR = Path(__file__).resolve().parents[2]
 KAGGLE_DATA_PATH = PARENT_DIR / 'data/raw/DATASET/TEST'
 MODEL_DIR = PARENT_DIR / 'models'
-def get_data(prediction_variable: str):
-    """Helper function to find all images of a certain class within data directory using metadata file"""
-    pass
+VIZ_DIR= PARENT_DIR / 'reports/figures'
 
-def plot_confusion_matrix(
-    model_path:Path = MODEL_DIR / '2019-08-28 08:03:49.h5',
-    prediction_variable:Union[str, Path]
-    ):
+
+def plot_confusion_matrix(university:str):
     """For kaggle data, prediction class is organized by folder structure, but for scraped data, sql metadata is used."""
-    clf = load_model(model_path)
-    y_hat = clf.predict(validation_generator)
+    X_train, y_train, X_val, y_val = train_val_split(university)
+    X_val = X_val.reshape((64, 244, 244, 3))
+    clf = tf.keras.load_model(MODEL_DIR / university)
+    y_hat = clf.predict_classes(X_val)
+    con_mat = tf.math.confusion_matrix(labels=y_val, predictions=y_hat).numpy()
+    con_mat_norm = np.around(
+        con_mat.astype('float') / con_mat.sum(axis=1)[:, np.newaxis], 
+        decimals=2
+        )
+    con_mat_df = pd.DataFrame(con_mat_norm, index=classes, columns=classes)
+    figure = plt.figure(figsize=(8,8))
+    sns.heatmap(con_mat_df, annot=True, cmap=plt.cm.Blues)
+    plt.tight_layout()
+    plt.xlabel('True label')
+    plt.ylabel('Predicted label')
+    plt.savefig(f'{university}_confusion_matrix.png')
+
+
+
