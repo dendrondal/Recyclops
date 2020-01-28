ada4428af2e9299eb368cb80a044e91883b9d218
deterministic-forest <jwilli18@go.olemiss.edu>
2020-01-27 18:50:24 -0600

    Added uni pate to templates

--- cif3r/models/predict_model.py
+++ cif3r/models/predict_model.py
@@ -5,10 +5,13 @@ from pathlib import Path
 from app import models
 
 
-def clf_factory(university, img_array)
+def clf_factory(university, img_array, labels)
     clf = tf.keras.models.load_model(Path(__file__).resolve().parents[1] / f'models/{university}.h5')
     x = np.expand_dims(img_array, axis=0)
-    clf.predict(x)
+    result = clf.predict(x)
+    index = np.where(result == np.amax(result))[0][0]
+    return labels[index]
+    
 
 
 
