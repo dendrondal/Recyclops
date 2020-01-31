5bc366117313038ebe6bfc5bc27d324e8337ae9f
Dal Williams <dendrondal@protonmail.com>
2020-01-31 17:08:04 -0600

    Refactored train_model into CLI

--- cif3r/models/predict_model.py
+++ cif3r/models/predict_model.py
@@ -3,13 +3,14 @@ from PIL import Image
 import numpy as np
 from pathlib import Path
 from app import models
-from cif3r.models.train_model import macro_f1_loss, macro_f1
+from cif3r.models.custom_metrics import macro_f1_loss, macro_f1
 
 custom_metrics = {'macro_f1_loss': macro_f1_loss, 'macro_f1': macro_f1}
 
+
 def clf_factory(university, img_array, labels):
-    print(university)
-    clf = tf.keras.models.load_model("/home/dal/CIf3R/models/{}.h5".format(university), custom_objects=custom_metrics)
+    model_dir = Path(__file__).resolve().parents[1]
+    clf = tf.keras.models.load_model(f'{model_dir}/{university}.h5', custom_objects=custom_metrics)
     x = np.expand_dims(img_array, axis=0)
     result = clf.predict(x)
     index = np.where(result == np.amax(result))[0][0]
